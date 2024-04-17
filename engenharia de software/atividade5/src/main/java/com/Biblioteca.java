f7f08a44cf9f6b31db36aa17b1b3539d4cd5acc0
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-04-17 00:10:10 -0300

    ajustando projeto

--- engenharia de software/atividade5/src/main/java/com/Biblioteca.java
+++ engenharia de software/atividade5/src/main/java/com/Biblioteca.java
@@ -1,11 +1,28 @@
 package com;
 
+import java.util.List;
+
 public class Biblioteca {
     public static void main(String[] args) {
-        Aluno aluno1 = new Aluno();
+        CadastrarAluno aluno1 = new CadastrarAluno();
+        CadastrarAluno aluno2 = new CadastrarAluno();
 
         aluno1.setNome("vinicius");
+        aluno1.setIdade("25");
+        aluno1.setDocumento("12321");
 
-        System.out.println(aluno1.getNome());
+        aluno2.setNome("bianca");
+        aluno2.setIdade("22");
+        aluno2.setDocumento("13231");
+
+        // Adiciona alunos à listaDeAlunos
+        List<String> todosAlunos = aluno1.todosAlunos();
+        todosAlunos.addAll(aluno2.todosAlunos());
+
+        // Imprime todos os alunos
+        System.out.println("Todos os alunos:");
+        for (String aluno : todosAlunos) {
+            System.out.println(aluno);
+        }
     }
 }
