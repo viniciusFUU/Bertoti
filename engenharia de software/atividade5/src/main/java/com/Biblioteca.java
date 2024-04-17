3c55a5da6c8fc5eeb9cc1d0c346651666f3994c0
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-04-16 23:55:13 -0300

    novo projeto

---
+++ engenharia de software/atividade5/src/main/java/com/Biblioteca.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Biblioteca {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+
+        aluno1.setNome("vinicius");
+
+        System.out.println(aluno1.getNome());
+    }
+}
