306fc71fc0ec83b1b5907339524c8037dff6b21d
Vinicius <vinicius3_1998@hotmail.com>
2024-09-05 09:52:02 -0300

    ajuste no database

--- database/database.sql
+++ database/database.sql
@@ -5,7 +5,7 @@ USE bertoti;
 CREATE TABLE usuario(
 	user_id INT PRIMARY KEY AUTO_INCREMENT,
     user_name VARCHAR(25),
-    user_agr VARCHAR(25),
+    user_age VARCHAR(25),
     user_gender VARCHAR(1)
 );
 
